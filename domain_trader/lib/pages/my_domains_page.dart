6bcdbe6316139b4b590fc3726a2c01d2750e5aee
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-10-30 16:15:29 -0300

    adicionado a página "meus domínios"

---
+++ domain_trader/lib/pages/my_domains_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyDomainsPage extends StatefulWidget {
+  const MyDomainsPage({super.key});
+
+  @override
+  State<MyDomainsPage> createState() => _MyDomainsPageState();
+}
+
+class _MyDomainsPageState extends State<MyDomainsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
