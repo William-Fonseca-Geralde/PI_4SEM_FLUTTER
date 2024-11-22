6776b54041203f1663b796b59c289c352523a651
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-11-22 11:30:57 -0300

    transferido o dialog.fullscreen() como um arquivo separado

--- domain_trader/lib/src/features/domains_lists/presentation/pages/add_domain_page.dart
+++ domain_trader/lib/src/features/domains_lists/presentation/pages/add_domain_page.dart
@@ -1,4 +1,4 @@
-import 'package:domain_trader/src/features/core/constants/constants.dart';
+import 'package:domain_trader/src/dialog_full.dart';
 import 'package:domain_trader/src/features/domains_lists/presentation/widgets/category_input.dart';
 import 'package:domain_trader/src/features/domains_lists/presentation/widgets/daterange_input.dart';
 import 'package:domain_trader/src/features/domains_lists/presentation/widgets/status_input.dart';
@@ -24,61 +24,15 @@ class _AddDomainPageState extends State<AddDomainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog.fullscreen(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Novo Domínio'),
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(Icons.close)
-          ),
-        ),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(paddingPadrao),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    logo_domain,
-                    width: 200,
-                    height: 200,
-                  ),
-                  const InputText(prefixIcon: Icon(CupertinoIcons.globe), hintText: 'Ex: www.teste.com', typeText: 'domain', labelText: 'Domínio'),
-                  const DaterangeInput(),
-                  const InputText(prefixIcon: Icon(Icons.attach_money), hintText: 'Ex: R\$ 99.99', typeText: 'price', labelText: 'Preço'),
-                  const StatusInput(),
-                  CategoryInput(dropdownMenuEntries: listaCategory),
-                  const SizedBox(height: paddingPadrao),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      FloatingActionButton.extended(
-                        label: const Text('Cancelar'),
-                        icon: const Icon(Icons.cancel_outlined),
-                        backgroundColor: cancelColor,
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                      ),
-                      FloatingActionButton.extended(
-                        label: const Text('Salvar'),
-                        icon: const Icon(Icons.save),
-                        onPressed: () {
-                          
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          )
-        ),
-      )
+    return DialogFull(
+      nomeDialog: 'Novo Domínio',
+      forms: [
+        const InputText(prefixIcon: Icon(CupertinoIcons.globe), hintText: 'Ex: www.teste.com', typeText: 'domain', labelText: 'Domínio'),
+        const DaterangeInput(),
+        const InputText(prefixIcon: Icon(Icons.attach_money), hintText: 'Ex: R\$ 99.99', typeText: 'price', labelText: 'Preço'),
+        const StatusInput(),
+        CategoryInput(dropdownMenuEntries: listaCategory)
+      ]
     );
   }
 }
